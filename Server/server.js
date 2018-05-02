6b16a8a89b568e4a4ab513cbd549b61bff816f1d
Nimesha Buddhika <135071J@uom.lk>
2018-05-02 18:27:03 +0530

    intigrate angular with node

--- Server/server.js
+++ Server/server.js
@@ -9,14 +9,16 @@ const port = 3030;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname+ 'dist')));
+// app.use(express.static(path.join(__dirname+ 'dist')));
+app.use(express.static(path.join('C:\\Users\\Nimesha Buddhika\\Documents\\PhpStrom Projects\\VideoApp\\Client\\VideoApp\\dist')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/api',api);
 app.use(cors());
 
 app.get('*',(req,res) => {
-   res.sendFile(path.join(__dirname,'dist/index.html'));
+   // res.sendFile(path.join(__dirname,'dist/index.html'));
+   res.sendFile(path.join('C:\\Users\\Nimesha Buddhika\\Documents\\PhpStrom Projects\\VideoApp\\Client\\VideoApp\\dist\\index.html'));
 });
 
 app.listen(port,function(){
